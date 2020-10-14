2dfa4272c83f918444e8fdc3a9240c8f21ac746f
Rob Randell <robrandell@heckford-advertising.co.uk>
2020-10-14 16:52:37 +0100

    feat: add `features` section

--- resources/views/planet.blade.php
+++ resources/views/planet.blade.php
@@ -137,6 +137,37 @@
                         </div>
                     </div>
                 </section>
+
+                <section class="features">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5 offset-md-4">
+                                <hr />
+                                <h2>Features</h2>
+                                <p>{{ $planet->overview->description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="background">
+                        <figure>
+                            <img src="/images/venus-clouds.jpg" class="img-fluid w-100 h-100" style="object-fit: cover;" alt="...">
+                        </figure>
+                        <div class="banner-gradient"></div>
+                        <div class="container position-relative py-5" style="z-index: 1;">
+                            <div class="row">
+                                <div class="col-md-5 offset-md-7">
+                                    <h3>Pearls</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <ul>
+                                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+                                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+                                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     </body>
