235d46fe565eedf0d4e6cdfa1d7ce398f64c7e8a
Rob Randell <robrandell@heckford-advertising.co.uk>
2020-10-15 09:35:19 +0100

    refactor: update `features` table structure

--- database/migrations/2020_10_06_134234_create_features_table.php
+++ database/migrations/2020_10_06_134234_create_features_table.php
@@ -15,8 +15,16 @@ class CreateFeaturesTable extends Migration
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->string('type');
+            $table->text('overview');
+            $table->string('bullet1')->nullable();
+            $table->string('bullet2')->nullable();
+            $table->string('bullet3')->nullable();
+            $table->string('bullet1_icon')->nullable();
+            $table->string('bullet2_icon')->nullable();
+            $table->string('bullet3_icon')->nullable();
+            $table->string('image_url');
             $table->morphs('featureable');
             $table->timestamps();
         });
