30e8594377bdf787cb859de0e64b6391277e6278
Rob Randell <robrandell@heckford-advertising.co.uk>
2020-10-15 13:47:52 +0100

    fix: lowercase function name

--- app/Models/Star.php
+++ app/Models/Star.php
@@ -14,6 +14,6 @@ class Star extends Model
     }
 
     public function environment() {
-        return $this->HasOne(Environment::class);
+        return $this->hasOne(Environment::class);
     }
 }
